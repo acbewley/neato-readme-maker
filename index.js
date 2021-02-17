57135a1c7608fbe26bb9b08aaab7c0c154e663e9
acbewley <acbewley13@gmail.com>
2021-02-17 15:59:41 -0500

    worked on init() and writeToFile()

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your project's title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Describe your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation instructions:",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Usage information:",
+        name: "usage"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        writeToFile("README.md", response)
+    })
+}
 
 // Function call to initialize app
 init();
